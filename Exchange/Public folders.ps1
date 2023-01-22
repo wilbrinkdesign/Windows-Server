ca6d542d26801b7f47719f360e9eb795ab9b29fb
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 21:11:08 +0100

    Update and rename Public folders.ps1 to Public folders.md

--- Exchange/Public folders.ps1
+++
@@ -1,7 +0,0 @@
-Get-OrganizationConfig | fl publicfoldersenabled
-New-Mailbox -PublicFolder -Name MasterHierachy
-
-Add-PublicFolderClientPermission "\Folder1\Adresboek1" -User "AD_GROEP" -AccessRights Reviewer
-Get-PublicFolder "\Folder1\Adresboek1" | Get-PublicFolderClientPermission
-
-Verdere permissies in orde maken? Open Outlook client en pas daar de machtigingen aan.
