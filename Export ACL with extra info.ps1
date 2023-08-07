66417a38749e95b0477bc77619d13c42a6a146cc
Mark Wilbrink <mark@wilbrink.design>
2023-08-07 20:12:50 +0200

    Add Export ACL with extra info.ps1

---
+++ Export ACL with extra info.ps1
@@ -0,0 +1,21 @@
+$Path = "<dir>"
+$Directory = Get-ChildItem $Path -Recurse
+$Complete_List = @()
+
+Foreach ($File in $Directory)
+{
+	$File_Path = $File.FullName
+	$File_Owner = ($File_Path | Get-Acl).Owner
+	$File_Permissions = ($File_Path | Get-Acl | select -ExpandProperty Access).FileSystemRights
+	$File_Date = $File.LastWriteTime
+
+	$List = New-Object -TypeName PSObject
+	$List | Add-Member -NotePropertyName Path -NotePropertyValue $File_Path
+	$List | Add-Member -NotePropertyName Owner -NotePropertyValue $File_Owner
+	$List | Add-Member -NotePropertyName Permissions -NotePropertyValue $File_Permissions
+	$List | Add-Member -NotePropertyName Date -NotePropertyValue $File_Date
+
+	$Complete_List += $List
+}
+
+$Complete_List | Out-GridView
